dbae2d5cda44a5a159e664c9175af23911854f88
giordifungula <jodi.fungula@gmail.com>
2019-11-13 22:35:12 +0200

    added styling to menu component

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4319ec5d2a0c5c3a702d";
+/******/ 	var hotCurrentHash = "d89bd696bc682fa05e8c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
